f003d5261573c149f95762a149df5460528718e0
Hassan Fatemi <hassan.fatemi@gmail.com>
2020-01-27 22:14:24 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love docker!");
+    res.json("I love docker! My build is gonna succeed!");
 });
 
 app.listen(PORT, () => {
